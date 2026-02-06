0b84177639296ad2c517ea9c268ed103684e75f3
Gowtham L <91368589+Gowtham843@users.noreply.github.com>
2026-02-06 11:56:31 +0530

    Add solution: array/1-two-sum.py

---
+++ array/1-two-sum.py
@@ -0,0 +1,11 @@
+"""
+ 1. Two Sum
+ URL: https://leetcode.com/problems/two-sum/description/
+ Difficulty: Easy
+ Topics: Array, Hash Table
+ Date: 2026-02-06T06:26:30.592Z
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
