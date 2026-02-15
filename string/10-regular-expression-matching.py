54d3c9c739fcab36b101553913dd54935308f77f
Gowtham L <91368589+Gowtham843@users.noreply.github.com>
2026-02-15 23:40:04 +0530

    Update solution: string/10-regular-expression-matching.py

--- string/10-regular-expression-matching.py
+++ string/10-regular-expression-matching.py
@@ -3,7 +3,7 @@
  URL: https://leetcode.com/problems/regular-expression-matching/
  Difficulty: Hard
  Topics: String, Dynamic Programming, Recursion
- Date: 2026-02-15T18:09:27.234Z
+ Date: 2026-02-15T18:09:53.152Z
 """
 
         dp = [[False] * (n + 1) for _ in range(m + 1)]
@@ -25,5 +25,7 @@
         
         return dp[m][n]
     def isMatch(self, s, p):
-        
+
+
 class Solution(object):
+        
