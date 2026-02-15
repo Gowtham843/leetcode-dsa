b978c150a6f4ff02f54c27a51f5530f5b55d1dcf
Gowtham L <91368589+Gowtham843@users.noreply.github.com>
2026-02-15 23:00:46 +0530

    Add solution: math/62-unique-paths.py

---
+++ math/62-unique-paths.py
@@ -0,0 +1,17 @@
+"""
+ 62. Unique Paths
+ URL: https://leetcode.com/problems/unique-paths/
+ Difficulty: Medium
+ Topics: Math, Dynamic Programming, Combinatorics
+ Date: 2026-02-15T17:30:34.563Z
+"""
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        dp = [1]*n
+        for _ in range(1,m):
+            for j in range(1,n):
+                dp[j]+=dp[j-1]
+        
+        return dp[-1]
+        
