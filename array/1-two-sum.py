14160d9e48cd4666e7ff565ef12d99166e3715fa
Gowtham L <91368589+Gowtham843@users.noreply.github.com>
2026-02-15 23:41:16 +0530

    Update solution: array/1-two-sum.py

--- array/1-two-sum.py
+++ array/1-two-sum.py
@@ -1,27 +1,19 @@
 """
  1. Two Sum
- URL: https://leetcode.com/problems/two-sum/description/
+ URL: https://leetcode.com/problems/two-sum/
  Difficulty: Easy
  Topics: Array, Hash Table
- Date: 2026-02-06T06:30:30.215Z
+ Date: 2026-02-15T18:11:05.265Z
 """
 
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        left=0
-        right=left+1
-        while right<len(nums):
-            if nums[left]+nums[right]==target:
-                return [left,right]
-            else:
-                right+=1
-                if right==len(nums):
-                    left+=1
-                    right=left+1
-        return [-1,-1]
+        hashmap = {}
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], index]
+            hashmap[num] = index
+        return [-1, -1]
+
         
